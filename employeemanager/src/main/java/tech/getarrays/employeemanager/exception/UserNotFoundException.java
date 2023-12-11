7f499bd645e282cd9af38c90634a783e73d6c7ee
GalileeWandji <146034249+GalileeWandji@users.noreply.github.com>
2023-12-11 21:29:33 +0100

    Add files via upload

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package tech.getarrays.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        Super(message);
+    }
+
+    private void Super(String message) {
+    }
+
+
+}
